0a23ae71e007a2b8723835e4ecc236a3a2a663bc
Julia Gonik <jgonik@mit.edu>
2018-12-05 00:35:03 -0500

    Teachable Machine Extension

--- appinventor/components/src/edu/mit/appinventor/ai/teachablemachine/TeachableMachine.java
+++ appinventor/components/src/edu/mit/appinventor/ai/teachablemachine/TeachableMachine.java
@@ -3,7 +3,55 @@
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-package com.google.appinventor.components.runtime;
+package edu.mit.appinventor.ai.teachablemachine;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Base64;
+import android.util.Log;
+import android.view.WindowManager.LayoutParams;
+import android.webkit.JavascriptInterface;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.DesignerProperty;
+import com.google.appinventor.components.annotations.PropertyCategory;
+import com.google.appinventor.components.annotations.SimpleEvent;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.SimpleProperty;
+import com.google.appinventor.components.annotations.UsesAssets;
+import com.google.appinventor.components.annotations.UsesPermissions;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.common.PropertyTypeConstants;
+import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
+import com.google.appinventor.components.runtime.Component;
+import com.google.appinventor.components.runtime.EventDispatcher;
+import com.google.appinventor.components.runtime.Form;
+import com.google.appinventor.components.runtime.WebViewer;
+import com.google.appinventor.components.runtime.util.ErrorMessages;
+import com.google.appinventor.components.runtime.util.JsonUtil;
+import com.google.appinventor.components.runtime.util.MediaUtil;
+import com.google.appinventor.components.runtime.util.SdkLevel;
+import com.google.appinventor.components.runtime.util.YailList;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.util.Log;
 import android.view.View;
@@ -27,16 +75,14 @@ import com.google.appinventor.components.runtime.util.YailList;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static android.net.Uri.encode;
 
@@ -47,13 +93,14 @@ import static android.net.Uri.encode;
  * @author kelseyc@mit.edu (Kelsey Chan)
  */
 
-@DesignerComponent(version = YaVersion.TEACHABLEMACHINE_COMPONENT_VERSION,
-        category = ComponentCategory.EXPERIMENTAL,
-        description = "Component for teaching a machine to recognize different images.")
-@SimpleObject
+@DesignerComponent(version = 1,
+        category = ComponentCategory.EXTENSION,
+		   description = "Extension for teaching a machine to recognize different images.",
+        nonVisible = true)
+@SimpleObject(external = true)
 @UsesAssets(fileNames = "teachablemachine.html, teachablemachine.js, group1-shard1of1, group10-shard1of1, group11-shard1of1, group12-shard1of1, group13-shard1of1, group14-shard1of1, group15-shard1of1, group16-shard1of1, group17-shard1of1, group18-shard1of1, group19-shard1of1, group2-shard1of1, group20-shard1of1, group21-shard1of1, group22-shard1of1, group23-shard1of1, group24-shard1of1, group25-shard1of1, group26-shard1of1, group27-shard1of1, group28-shard1of1, group29-shard1of1, group3-shard1of1, group30-shard1of1, group31-shard1of1, group32-shard1of1, group33-shard1of1, group34-shard1of1, group35-shard1of1, group36-shard1of1, group37-shard1of1, group38-shard1of1, group39-shard1of1, group4-shard1of1, group40-shard1of1, group41-shard1of1, group42-shard1of1, group43-shard1of1, group44-shard1of1, group45-shard1of1, group46-shard1of1, group47-shard1of1, group48-shard1of1, group49-shard1of1, group5-shard1of1, group50-shard1of1, group51-shard1of1, group52-shard1of1, group53-shard1of1, group54-shard1of1, group55-shard1of1, group6-shard1of1, group7-shard1of1, group8-shard1of1, group9-shard1of1, model.json, tfjs-0.10.3.js")
 @UsesPermissions(permissionNames = "android.permission.INTERNET, android.permission.CAMERA")
-public final class TeachableMachine extends AndroidViewComponent implements Component {
+public final class TeachableMachine extends AndroidNonvisibleComponent implements Component {
   private static final String LOG_TAG = TeachableMachine.class.getSimpleName();
   private static final String MODEL_DIRECTORY = "/sdcard/AppInventor/assets/TeachableMachine/";
 
@@ -67,55 +114,88 @@ public final class TeachableMachine extends AndroidViewComponent implements Comp
   private static final int ERROR_SAVE_MODEL_FAILED = -5;
   private static final int ERROR_NO_MORE_CLASSES_AVAILABLE = -6;
   private static final int ERROR_LABEL_DOES_NOT_EXIST = -7;
+  private static final int ERROR_WEBVIEWER_REQUIRED = -8;
 
-  private final WebView webview;
+  private WebView webview;
   private final Form form;
 
-  public TeachableMachine(ComponentContainer container) {
+  public TeachableMachine(Form container) {
     super(container);
     this.form = container.$form();
-    webview = new WebView(container.$context());
-    webview.getSettings().setJavaScriptEnabled(true);
-    webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
-    // adds a way to send strings to the javascript
-    webview.addJavascriptInterface(new JsObject(), "TeachableMachine");
-    webview.setWebViewClient(new WebViewClient() {
-      @Override
-      public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-        Log.d(LOG_TAG, "shouldInterceptRequest called");
-        if (url.contains(MODEL_PREFIX)) {
-          Log.d(LOG_TAG, "overriding " + url);
-          try {
-            InputStream inputStream = form.$context().getAssets().open("component/" + url.substring(MODEL_PREFIX.length()));
-            if (url.endsWith(".json")) {
-              return new WebResourceResponse("application/json", "UTF-8", inputStream);
-            } else {
-              return new WebResourceResponse("application/octet-stream", "binary", inputStream);
-            }
-          } catch (IOException e) {
-            e.printStackTrace();
-            return super.shouldInterceptRequest(view, url);
-          }
-        }
-        Log.d(LOG_TAG, url);
-        return super.shouldInterceptRequest(view, url);
-      }
-    });
-    webview.setWebChromeClient(new WebChromeClient() {
-      @Override
-      public void onPermissionRequest(PermissionRequest request) {
-        Log.d(LOG_TAG, "onPermissionRequest called");
-        String[] requestedResources = request.getResources();
-        for (String r : requestedResources) {
-          if (r.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
-            request.grant(new String[]{PermissionRequest.RESOURCE_VIDEO_CAPTURE});
-          }
-        }
-      }
-    });
-    webview.loadUrl("file:///android_asset/component/teachablemachine.html");
+    requestHardwareAcceleration(container);
     Log.d(LOG_TAG, "Created TeachableMachine component");
-    container.$add(this);
+  }
+  
+  @SuppressLint("SetJavaScriptEnabled")
+  private void configureWebView(WebView webview) {
+	  webview.getSettings().setJavaScriptEnabled(true);
+	  webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
+	    // adds a way to send strings to the javascript
+	  webview.addJavascriptInterface(new JsObject(), "TeachableMachine");
+	  webview.setWebViewClient(new WebViewClient() {
+	      @Override
+	      public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+	        Log.d(LOG_TAG, "shouldInterceptRequest called");
+	        if (url.contains(MODEL_PREFIX)) {
+	          Log.d(LOG_TAG, "overriding " + url);
+	          try {
+	            InputStream inputStream = form.$context().getAssets().open("component/" + url.substring(MODEL_PREFIX.length()));
+	            if (url.endsWith(".json")) {
+	              return new WebResourceResponse("application/json", "UTF-8", inputStream);
+	            } else {
+	              return new WebResourceResponse("application/octet-stream", "binary", inputStream);
+	            }
+	          } catch (IOException e) {
+	            e.printStackTrace();
+	            return super.shouldInterceptRequest(view, url);
+	          }
+	        }
+	        Log.d(LOG_TAG, url);
+	        return super.shouldInterceptRequest(view, url);
+	      }
+	    });
+	  webview.setWebChromeClient(new WebChromeClient() {
+	      @Override
+	      public void onPermissionRequest(PermissionRequest request) {
+	        Log.d(LOG_TAG, "onPermissionRequest called");
+	        String[] requestedResources = request.getResources();
+	        for (String r : requestedResources) {
+	          if (r.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
+	            request.grant(new String[]{PermissionRequest.RESOURCE_VIDEO_CAPTURE});
+	          }
+	        }
+	      }
+	    });
+	  try {
+	    webview.loadUrl(form.getAssetPathForExtension(this, "teachablemachine.html"));
+	  } catch (FileNotFoundException e) {
+        Log.d(LOG_TAG, e.getMessage());
+        e.printStackTrace();
+      }
+  }
+  
+  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_COMPONENT + ":com.google.appinventor.runtime.components.WebViewer")
+  @SimpleProperty(userVisible = false)
+  public void WebViewer(WebViewer webviewer) {
+	  if (webviewer != null) {
+	    webview = (WebView) webviewer.getView();
+		  configureWebView(webview);
+		  webview.requestLayout();
+		  try {
+			  Log.d(LOG_TAG, "isHardwareAccelerated? " + webview.isHardwareAccelerated());
+			  webview.loadUrl(form.getAssetPathForExtension(this, "teachablemachine.html"));
+		  } catch (FileNotFoundException e) {
+			  Log.d(LOG_TAG, e.getMessage());
+			  e.printStackTrace();
+		  }
+	  }
+  }
+  
+  public void Initialize() {
+	  Log.d(LOG_TAG, "webview = " + webview);
+	  if (webview == null) {
+		  form.dispatchErrorOccurredEvent(this, "WebViewer", ErrorMessages.ERROR_EXTENSION_ERROR, ERROR_WEBVIEWER_REQUIRED, LOG_TAG, "You must specify a WebViewer component in the WebViewer property.");
+	  }
   }
 
   @SimpleFunction(description = "Toggles between user-facing and environment-facing camera.")
@@ -189,6 +269,10 @@ public final class TeachableMachine extends AndroidViewComponent implements Comp
     EventDispatcher.dispatchEvent(this, "Error", errorCode, message);
   }
 
+  private static void requestHardwareAcceleration(Activity activity) {
+    activity.getWindow().setFlags(LayoutParams.FLAG_HARDWARE_ACCELERATED, LayoutParams.FLAG_HARDWARE_ACCELERATED);
+  }
+
   private void saveModel(String name, String model) {
     // save to file system
     String path = MODEL_DIRECTORY + name;
@@ -206,10 +290,6 @@ public final class TeachableMachine extends AndroidViewComponent implements Comp
     DoneSavingModel(name);
   }
 
-  @Override
-  public View getView() {
-    return webview;
-  }
 
   private class JsObject {
     @JavascriptInterface
---
+++ appinventor/components/src/edu/mit/appinventor/ai/teachablemachine/assets/teachablemachine.js
@@ -0,0 +1,493 @@
+"use strict";
+
+console.log("TeachableMachine: Using TensorFlow.js version " + tf.version.tfjs);
+
+const MOBILENET_MODEL_PATH = "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json";
+
+const NUM_CLASSES = 50;
+const IMAGE_SIZE = 224;
+const TOPK = 10;
+const MAX_EXAMPLES = 50;
+
+// make sure error codes are consistent with those defined in TeachableMachine.java
+const ERROR_CLASSIFICATION_NOT_SUPPORTED = -1;
+const ERROR_CLASSIFICATION_FAILED = -2;
+const ERROR_LOAD_MODEL_FAILED_BAD_FILE_FORMAT = -3;
+const ERROR_LOAD_MODEL_FAILED_TOO_MANY_CLASSES = -4;
+const ERROR_SAVE_MODEL_FAILED = -5;
+const ERROR_NO_MORE_CLASSES_AVAILABLE = -6;
+const ERROR_LABEL_DOES_NOT_EXIST = -7;
+
+class KNNImageClassifier {
+  constructor(numClasses, k) {
+    this.numClasses = numClasses;
+    this.k = k;
+    this.classLogitsMatrices = [];
+    this.classExampleCount = [];
+    this.varsLoaded = false;
+    this.squashLogitsDenominator = tf.scalar(300);
+    for (let i = 0; i < this.numClasses; i++) {
+      this.classLogitsMatrices.push(null);
+      this.classExampleCount.push(0);
+    }
+  }
+
+  async load() {
+    try {
+      this.mobilenet = await tf.loadModel(MOBILENET_MODEL_PATH);
+      const zeros = tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+      this.mobilenet.predict(zeros).dispose();
+      zeros.dispose();
+      this.varsLoaded = true;
+      console.log("TeachableMachine: KNNImageClassifier ready");
+    } catch (error) {
+      console.log("TeachableMachine: " + error);
+      TeachableMachine.error(ERROR_CLASSIFICATION_NOT_SUPPORTED, "");
+    }
+  }
+
+  clearClass(classIndex) {
+    if (classIndex >= this.numClasses) {
+      console.log(`Cannot clear invalid class ${classIndex}`);
+      return;
+    }
+    this.classLogitsMatrices[classIndex].dispose();
+    this.classLogitsMatrices[classIndex] = null;
+    this.classExampleCount[classIndex] = 0;
+    this._clearTrainLogitsMatrix();
+  }
+
+  addImage(image, classIndex) {
+    if (!this.varsLoaded) {
+      console.warn("Cannot add images until vars have been loaded.");
+      return;
+    }
+    if (classIndex >= this.numClasses) {
+      console.warn(`Cannot add to invalid class ${classIndex}`);
+      return;
+    }
+    this._clearTrainLogitsMatrix();
+    try {
+      tf.tidy(() => {
+        const logits = tf.tidy(() => {
+          const offset = tf.scalar(127.5);
+          const normalized = image.sub(offset).div(offset);
+          const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+          return this.mobilenet.predict(batched);
+        });
+        const imageLogits = this._normalizeVector(logits);
+        const logitsSize = imageLogits.shape[1];
+        if (this.classLogitsMatrices[classIndex] == null) {
+          this.classLogitsMatrices[classIndex] = imageLogits.as2D(1, logitsSize);
+        } else {
+          const newTrainLogitsMatrix =
+              this.classLogitsMatrices[classIndex]
+                  .as2D(this.classExampleCount[classIndex], logitsSize)
+                  .concat(imageLogits.as2D(1, logitsSize), 0);
+          this.classLogitsMatrices[classIndex].dispose();
+          this.classLogitsMatrices[classIndex] = newTrainLogitsMatrix;
+        }
+        tf.keep(this.classLogitsMatrices[classIndex]);
+        this.classExampleCount[classIndex]++;
+      });
+    } catch (error) {
+      console.log("TeachableMachine: " + error);
+      TeachableMachine.error(ERROR_CLASSIFICATION_NOT_SUPPORTED, "");
+    }
+  }
+
+  predict(image) {
+    if (!this.varsLoaded) {
+      throw new Error("Cannot predict until vars have been loaded.");
+    }
+    try {
+      return tf.tidy(() => {
+        const logits = tf.tidy(() => {
+          const offset = tf.scalar(127.5);
+          const normalized = image.sub(offset).div(offset);
+          const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+          return this.mobilenet.predict(batched);
+        });
+        const imageLogits = this._normalizeVector(logits);
+        const logitsSize = imageLogits.shape[1];
+        if (this.trainLogitsMatrix == null) {
+          let newTrainLogitsMatrix = null;
+          for (let i = 0; i < this.numClasses; i++) {
+            newTrainLogitsMatrix = this._concatWithNulls(
+                newTrainLogitsMatrix, this.classLogitsMatrices[i]);
+          }
+          this.trainLogitsMatrix = newTrainLogitsMatrix;
+        }
+        if (this.trainLogitsMatrix == null) {
+          console.warn("Cannot predict without providing training images.");
+          return null;
+        }
+        tf.keep(this.trainLogitsMatrix);
+        const numExamples = this._getNumExamples();
+        return this.trainLogitsMatrix.as2D(numExamples, logitsSize)
+            .matMul(imageLogits.as2D(logitsSize, 1))
+            .as1D();
+      });
+    } catch (error) {
+      console.log("TeachableMachine: " + error);
+      TeachableMachine.error(ERROR_CLASSIFICATION_NOT_SUPPORTED, "");
+      return null;
+    }
+  }
+
+  async predictClass(image) {
+    let imageClass = -1;
+    const confidences = new Array(this.numClasses);
+    if (!this.varsLoaded) {
+      throw new Error("Cannot predict until vars have been loaded.");
+    }
+    const knn = tf.tidy(() => {
+      return this.predict(image).asType("float32");
+    });
+    const numExamples = this._getNumExamples();
+    const kVal = Math.min(this.k, numExamples);
+    const topK = this._topK(await knn.data(), kVal);
+    knn.dispose();
+    const topKIndices = topK.indices;
+    if (topKIndices == null) {
+      return {classIndex: imageClass, confidences};
+    }
+    const indicesForClasses = [];
+    const topKCountsForClasses = [];
+    for (let i = 0; i < this.numClasses; i++) {
+      topKCountsForClasses.push(0);
+      let num = this.classExampleCount[i];
+      if (i > 0) {
+        num += indicesForClasses[i - 1];
+      }
+      indicesForClasses.push(num);
+    }
+    for (let i = 0; i < topKIndices.length; i++) {
+      for (let classForEntry = 0; classForEntry < indicesForClasses.length;
+           classForEntry++) {
+        if (topKIndices[i] < indicesForClasses[classForEntry]) {
+          topKCountsForClasses[classForEntry]++;
+          break;
+        }
+      }
+    }
+    let topConfidence = 0;
+    for (let i = 0; i < this.numClasses; i++) {
+      const probability = topKCountsForClasses[i] / kVal;
+      if (probability > topConfidence) {
+        topConfidence = probability;
+        imageClass = i;
+      }
+      confidences[i] = probability;
+    }
+    return {classIndex: imageClass, confidences};
+  }
+
+  _topK(values, k) {
+    const valuesAndIndices = [];
+    for (let i = 0; i < values.length; i++) {
+      valuesAndIndices.push({value: values[i], index: i});
+    }
+    valuesAndIndices.sort((a, b) => {
+      return b.value - a.value;
+    });
+    const topkValues = new Float32Array(k);
+    const topkIndices = new Int32Array(k);
+    for (let i = 0; i < k; i++) {
+      topkValues[i] = valuesAndIndices[i].value;
+      topkIndices[i] = valuesAndIndices[i].index;
+    }
+    return {values: topkValues, indices: topkIndices};
+  }
+
+  getClassExampleCount() {
+    return this.classExampleCount;
+  }
+
+  getClassLogitsMatrices() {
+    return this.classLogitsMatrices;
+  }
+
+  setClassLogitsMatrices(classLogitsMatrices) {
+    this.classLogitsMatrices = classLogitsMatrices;
+    this.classExampleCount = classLogitsMatrices.map(
+        tensor => tensor != null ? tensor.shape[0] : 0);
+    this._clearTrainLogitsMatrix();
+  }
+
+  _clearTrainLogitsMatrix() {
+    if (this.trainLogitsMatrix != null) {
+      this.trainLogitsMatrix.dispose();
+      this.trainLogitsMatrix = null;
+    }
+  }
+
+  _concatWithNulls(ndarray1, ndarray2) {
+    if (ndarray1 == null && ndarray2 == null) {
+      return null;
+    }
+    if (ndarray1 == null) {
+      return ndarray2.clone();
+    } else if (ndarray2 === null) {
+      return ndarray1.clone();
+    }
+    return ndarray1.concat(ndarray2, 0);
+  }
+
+  _normalizeVector(vec) {
+    const squashedVec = tf.div(vec, this.squashLogitsDenominator);
+    const sqrtSum = squashedVec.square().sum().sqrt();
+    return tf.div(squashedVec, sqrtSum);
+  }
+
+  _getNumExamples() {
+    let total = 0;
+    for (let i = 0; i < this.classExampleCount.length; i++) {
+      total += this.classExampleCount[i];
+    }
+    return total;
+  }
+
+  dispose() {
+    this.mobilenet.dispose();
+    this._clearTrainLogitsMatrix();
+    this.classLogitsMatrices.forEach(
+        classLogitsMatrix => classLogitsMatrix.dispose());
+    this.squashLogitsDenominator.dispose();
+  }
+
+  loadLogits(logits, classIndex) {
+    if (!this.varsLoaded) {
+      console.warn("Cannot add images until vars have been loaded.");
+      return;
+    }
+    if (classIndex >= this.numClasses) {
+      console.warn(`Cannot add to invalid class ${classIndex}`);
+      return;
+    }
+    this._clearTrainLogitsMatrix();
+    if (this.classLogitsMatrices[classIndex] !== null) {
+      this.classLogitsMatrices[classIndex].dispose();
+    }
+    this.classLogitsMatrices[classIndex] = logits;
+    this.classExampleCount[classIndex] = logits.shape[0];
+  }
+}
+
+var training = -1;
+var videoPlaying = false;
+
+var knn = new KNNImageClassifier(NUM_CLASSES, TOPK);
+
+var video = document.createElement("video");
+video.setAttribute("autoplay", "");
+video.setAttribute("playsinline", "");
+video.width = window.innerWidth;
+video.style.display = "block";
+
+var frontFacing = false;
+
+document.body.appendChild(video);
+
+var timer;
+
+var labelToClass = {};
+var classToLabel = {};
+
+var confidences = {};
+
+var topChoice;
+
+var availableClasses = [];
+
+for (let i = 0; i < NUM_CLASSES; i++) {
+  availableClasses.push(i);
+}
+
+video.addEventListener("loadedmetadata", function () {
+  video.height = this.videoHeight * video.width / this.videoWidth;
+}, false);
+
+function startVideo() {
+  navigator.mediaDevices.getUserMedia({video: {facingMode: frontFacing ? "user" : "environment"}, audio: false})
+  .then(stream => {
+    video.srcObject = stream;
+    video.addEventListener("playing", () => videoPlaying = true);
+    video.addEventListener("paused", () => videoPlaying = false);
+  }).catch(e => log(e));
+}
+
+function stopVideo() {
+  if (video.srcObject) {
+    video.srcObject.getTracks().forEach(t => t.stop());
+  }
+}
+
+function toggleCameraFacingMode() {
+  frontFacing = !frontFacing;
+  stopVideo();
+  startVideo();
+}
+
+startVideo();
+
+knn.load()
+.then(() => {
+  start();
+  TeachableMachine.ready();
+});
+
+function start() {
+  if (timer) {
+    stop();
+  }
+  video.play();
+  timer = requestAnimationFrame(animate);
+}
+
+function stop() {
+  video.pause();
+  cancelAnimationFrame(timer);
+}
+
+function listSampleCounts() {
+  var sampleCounts = knn.getClassExampleCount();
+  var sList = [];
+  for (let i = 0; i < NUM_CLASSES; i++) {
+    if (classToLabel.hasOwnProperty(i) && sampleCounts.hasOwnProperty(i)) {
+      var classAndCount = [];
+      classAndCount.push(classToLabel[i]);
+      classAndCount.push(sampleCounts[i]);
+      sList.push(classAndCount);
+    }
+  }
+  return sList;
+}
+
+function listConfidences() {
+  var cList = [];
+  for (let i = 0; i < NUM_CLASSES; i++) {
+    if (classToLabel.hasOwnProperty(i) && confidences.hasOwnProperty(i)) {
+      var classAndConfidence = [];
+      classAndConfidence.push(classToLabel[i]);
+      classAndConfidence.push(confidences[i]);
+      cList.push(classAndConfidence);
+    }
+  }
+  return cList;
+}
+
+function animate() {
+  if(videoPlaying) {
+    const image = tf.tidy(() => {
+      return tf.image.resizeBilinear(tf.fromPixels(video).toFloat(), [IMAGE_SIZE, IMAGE_SIZE]);
+    });
+    if(training != -1 && knn.getClassExampleCount()[training] < MAX_EXAMPLES) {
+      knn.addImage(image, training);
+      TeachableMachine.gotSampleCounts(JSON.stringify(listSampleCounts()));
+    }
+    const exampleCount = knn.getClassExampleCount();
+    if(Math.max(...exampleCount) > 0) {
+      knn.predictClass(image)
+      .then(res => {
+        for(let i = 0; i < NUM_CLASSES; i++) {
+          if(res.classIndex === i) {
+            topChoice = i;
+          }
+          if(exampleCount[i] > 0) {
+            confidences[i] = res.confidences[i];
+          }
+        }
+        TeachableMachine.gotConfidences(JSON.stringify(listConfidences()));
+        TeachableMachine.gotClassification(classToLabel[topChoice]);
+      })
+      .then(() => image.dispose());
+    } else {
+      image.dispose();
+    }
+  }
+  timer = requestAnimationFrame(animate);
+}
+
+function startTraining(encodedLabel) {
+  var label = decodeURIComponent(encodedLabel);
+  if (!labelToClass.hasOwnProperty(label)) {
+    if (availableClasses.length == 0) {
+      TeachableMachine.error(ERROR_NO_MORE_CLASSES_AVAILABLE, label);
+      return;
+    }
+    var c = availableClasses.shift();
+    labelToClass[label] = c;
+    classToLabel[c] = label;
+  }
+  training = labelToClass[label];
+}
+
+function stopTraining() {
+  training = -1;
+}
+
+function clear(encodedLabel) {
+  var label = decodeURIComponent(encodedLabel);
+  if (!labelToClass.hasOwnProperty(label)) {
+    TeachableMachine.error(ERROR_LABEL_DOES_NOT_EXIST, label);
+    return;
+  }
+  if (training === labelToClass[label]) {
+    stopTraining();
+  }
+  knn.clearClass(labelToClass[label]);
+  availableClasses.push(labelToClass[label]);
+  availableClasses.sort((a, b) => a - b);
+  delete classToLabel[labelToClass[label]];
+  delete confidences[labelToClass[label]];
+  delete labelToClass[label];
+  TeachableMachine.gotSampleCounts(JSON.stringify(listSampleCounts()));
+  TeachableMachine.gotConfidences(JSON.stringify(listConfidences()));
+  if (classToLabel.hasOwnProperty(topChoice)) {
+    TeachableMachine.gotClassification(classToLabel[topChoice]);
+  } else {
+    TeachableMachine.gotClassification("");
+  }
+}
+
+window.addEventListener("resize", function() {
+  video.width = window.innerWidth;
+  video.height = video.videoHeight * window.innerWidth / video.videoWidth;
+});
+
+async function saveModel(encodedName) {
+  var classes = [];
+  for (var i = 0; i < NUM_CLASSES; i++) {
+    if (classToLabel.hasOwnProperty(i)) {
+      classes.push(classToLabel[i]);
+      classes.push(Array.from(await knn.classLogitsMatrices[i].data()));
+    }
+  }
+  TeachableMachine.gotSavedModel(decodeURIComponent(encodedName), JSON.stringify(classes));
+}
+
+function loadModel(encodedName, model) {
+  var name = decodeURIComponent(encodedName);
+  var array = JSON.parse(decodeURIComponent(model));
+  if (array.length > 2 * NUM_CLASSES) {
+    TeachableMachine.error(ERROR_LOAD_MODEL_FAILED_TOO_MANY_CLASSES, name);
+    return;
+  }
+  for (var i = 0; i < NUM_CLASSES; i++) {
+    if (classToLabel.hasOwnProperty(i)) {
+      clear(classToLabel[i]);
+    }
+  }
+  for (var i = 0; i < array.length / 2; i++) {
+    var label = array[2 * i];
+    var data = array[2 * i + 1];
+    var tensor = tf.tensor2d(data, [data.length / 1000, 1000]);
+    var c = availableClasses.shift();
+    knn.loadLogits(tensor, c);
+    labelToClass[label] = c;
+    classToLabel[c] = label;
+  }
+  TeachableMachine.gotSampleCounts(JSON.stringify(listSampleCounts()));
+  TeachableMachine.doneLoadingModel(name);
+}
